e3e0ec583f94926c54e816ba22c847991cca188a
liu kaixin <1360517661@qq.com>
2022-07-25 09:52:02 +0800

    fix deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,6 +3,12 @@ echo deploy project git pull
 # 拉取最新代码
 git pull
 
+cd react-auto
+
+npm config set registry https://registry.npm.taobao.org/ && npm i && npm run bild
+
+cd ..
+
 # echo git pull end
 
 # 停止运行的容器，并且会删除已停止的容器以及已创建的所有网络
